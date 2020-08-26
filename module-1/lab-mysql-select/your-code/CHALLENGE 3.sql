2a6207b7e538c684a843b3a86edf775b407060e3
Diego Caulonga <diegocaulonga@MacBook-Air-de-Diego-3.local>
2020-08-27 00:28:22 +0200

    challenge 3

--- module-1/lab-mysql-select/your-code/CHALLENGE 3.sql
+++ module-1/lab-mysql-select/your-code/CHALLENGE 3.sql
@@ -1,10 +1,7 @@
 SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME", sum(sales.qty) AS "TOTAL"
-	FROM authors
-		LEFT JOIN stores ON stores.city = authors.city
-		LEFT JOIN sales ON sales.stor_id = stores.stor_id
-				GROUP BY authors.au_id, authors.au_lname, authors.au_fname
-                ORDER BY ASC limit 3;
-					
-            
-        
- 
+FROM authors 
+LEFT JOIN store ON authors.city=stores.city
+LEFT JOIN sales ON sales.stor_id=stores.stor_id
+GROUP BY authors.au_id,authors.au_lname,authors.au_fname
+ORDER BY "TOTAL" DESC 
+limit 3
